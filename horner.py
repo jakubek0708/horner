ba743114c729c8b530405d18fe9dea3a1535d5d0
jakubek0708 <jakubek0708@gmail.com>
2021-11-08 08:18:30 +0100

    cosmetic changes

--- horner.py
+++ horner.py
@@ -1,25 +1,26 @@
-try: 
+try:
     stopien = int(input("podaj stopień wielomianu: "))
 
-    czynniki_wielomianu = [int(input(f"podaj czynnik przy potędze {-i}: ")) for i in range(-stopien, 1)]
+    czynniki_wielomianu = [
+        int(input(f"podaj czynnik przy potędze {-i}: ")) for i in range(-stopien, 1)]
 
     liczba_zerująca = int(input("podaj liczbe zerującą: "))
-    
-except ValueError: 
-    from sys import exit 
+
+except ValueError:
+    from sys import exit
     print("Podana wartość nie jest liczbą")
     exit(0)
 
-nowy_czynniki = [czynniki_wielomianu[0]]
+wyniki = [czynniki_wielomianu[0]]
 
 print("\nOdpowiedzi: ")
 for i in range(1, len(czynniki_wielomianu)):
 
-    wynik_czynnik = liczba_zerująca*nowy_czynniki[i-1]+czynniki_wielomianu[i]
+    wynik_czynnik = liczba_zerująca*wyniki[i-1]+czynniki_wielomianu[i]
 
-    nowy_czynniki.append(wynik_czynnik)
+    wyniki.append(wynik_czynnik)
 
-for i in range(-len(nowy_czynniki), -1):
-    print(f"wspołczynnik przy potędze {abs(i+2)}: {nowy_czynniki[i]}")
+for i in range(-len(wyniki), -1):
+    print(f"wspołczynnik przy potędze {abs(i+2)}: {wyniki[i]}")
 
-print(f"reszta: {nowy_czynniki[-1]}")
+print(f"reszta: {wyniki[-1]}")
